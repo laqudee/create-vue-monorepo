8cb76f30fd98ce823aa7ca502a5acb69a3f4edca
laqudee <2268678583@qq.com>
2023-06-13 21:38:27 +0800

    feat & fix some bugs

--- template/code/packages/common-library/src/index.js
+++ template/code/packages/common-library/src/index.js
@@ -1,6 +1,3 @@
-import { createAxios } from './axios/common.js'
-import { createEncrypter } from './encrypt/common.js'
 import * as validator from './validator/common.js'
-import * as domSetting from './dom/common.js'
 
-export { createAxios, createEncrypter, validator, domSetting }
+export { validator }
